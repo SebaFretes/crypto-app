91d5a1f64b83474e6ecdfa32b3b9283e268e6cfe
sebafretesgbc <sebastianf@globalbridgeconnections.com>
2025-11-05 17:30:09 -0300

    add styles in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+
+        <div className="content">
+        </div>
+      </div>
     </>
   )
 }
