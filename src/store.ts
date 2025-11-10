60827ecee3460511c27909df179121af830ffe10
sebafretesgbc <sebastianf@globalbridgeconnections.com>
2025-11-10 11:52:36 -0300

    update useCryptoStore function in store.ts

--- src/store.ts
+++ src/store.ts
@@ -19,6 +19,7 @@ export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
         }))
     },
     fetchData: async (pair) => {
-        fetchCurrentCryptoPrice(pair);
+        const result = await fetchCurrentCryptoPrice(pair);
+        console.log(result);
     }
 })))
